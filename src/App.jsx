574d1440d5d39344dfe5f4c73940ebe6efc3e97e
Jeremias Cane <canejere@gmail.com>
2022-06-02 11:56:24 -0300

    added Speech and MainComponentAbout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Navbar from "../src/Components/Navbar";
 import SleeveSlogan from './Components/SleeveSlogan'
+import Speech from "./Components/Speech";
 import '../src/Styles/TopSleeve.css'
+import MainComponentAbout from "./Components/MainComponentAbout";
 
 function App() {
   let grey = "B8B8B8"
@@ -10,6 +12,7 @@ function App() {
       <Navbar/>
       <SleeveSlogan/>
     </div>
+  
     </>
   );
 }
--- src/Components/Speech.jsx
+++ src/Components/Speech.jsx
@@ -1,8 +1,16 @@
+import '../Styles/speech.css'
+
 function Speech () {
     return(
-        <div>
-            
+        <div className="speech">
+            <div className='container-speech'>
+            "Original y con una comprensión innata de las necesidades de sus clientes, 
+            siempre es un placer trabajar con el equipo de Love Nature".            
+            </div>
+            <div className='container-photo'>
+                <img src={require('../img/face.png')} alt="Face" />
+            </div>
         </div>
     )
 }
-export default Speech
+export default Speech;
