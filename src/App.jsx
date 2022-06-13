c7d087dcc3a256ea80803c97fc5cf40e7546afea
felipemiguelsainz <78056889+felipemiguelsainz@users.noreply.github.com>
2022-06-13 18:50:08 -0300

    fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
       <Navbar/>
       <Routes>
-          <Route index element={<Home/>}/>
+          <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
       </Routes>
       <Footer/>
