8afd703bd6b1d3dae00588555a2f9d8b64642921
felipemiguelsainz <78056889+felipemiguelsainz@users.noreply.github.com>
2022-06-13 18:43:13 -0300

    commit navbar fixed 4.0

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
       <Navbar/>
       <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route index element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
       </Routes>
       <Footer/>
